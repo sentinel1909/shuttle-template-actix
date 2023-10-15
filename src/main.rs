f27e6f6706a416322079db39ab5710f0abf166ec
Jeff Mitchell <sentinel1909@jeff-mitchell.dev>
2023-10-14 20:55:40 -0700

    improvement: add tracing, not_found endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,57 @@
 // src/main.rs
 
 // dependencies
-use actix_web::{get, web::ServiceConfig, HttpResponse, Responder};
+use actix_cors::Cors;
+use actix_web::{
+    get,
+    web::{self, ServiceConfig},
+    HttpResponse, Responder,
+};
 use shuttle_actix_web::ShuttleActixWeb;
-use tracing::info;
+use tracing_actix_web::TracingLogger;
+use tracing_subscriber::filter::{EnvFilter, LevelFilter};
 
 // health_check endpoint
+#[tracing::instrument(name = "health_check")]
 #[get("/health_check")]
 async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
+    HttpResponse::Ok().json("Healthy")
+}
+
+// not found endpoint
+#[tracing::instrument(name = "not_found")]
+async fn not_found() -> impl Responder {
+    HttpResponse::NotFound().json("Not Found")
 }
 
 // main function, annotated for Shuttle
 #[shuttle_runtime::main]
 async fn actix_web() -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
+    // set up tracing, use tracing::fmt subscriber to write to stdout
+    tracing_subscriber::fmt()
+        .with_env_filter(
+            EnvFilter::builder()
+                .with_default_directive(LevelFilter::INFO.into())
+                .from_env_lossy(),
+        )
+        .init();
+    tracing::info!("Tracing initialized");
+
     let config = move |cfg: &mut ServiceConfig| {
-        cfg.service(health_check);
+        // CORS configuration is preset to permissive, change this in real life!!
+        let cors = Cors::permissive();
+
+        // configure the service, under the /api scope, with tracing, CORS, the health_check endpoint, and a default not_found endpoint
+        cfg.service(
+            web::scope("/api")
+                .wrap(cors)
+                .wrap(TracingLogger::default())
+                .service(health_check)
+                .default_service(web::route().to(not_found)),
+        );
     };
-    info!("Health check endpoint is at: /health_check");
+
+    // start the server
+    tracing::info!("Starting server");
     Ok(config.into())
 }
