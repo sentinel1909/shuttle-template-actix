948b6c5ab07e87224c1eae943942840becabe655
Jeff Mitchell <sentinel6671@outlook.com>
2024-02-03 09:45:31 -0800

    improvement: finish implementation of health_check test

--- tests/api/health_check.rs
+++ tests/api/health_check.rs
@@ -1,15 +1,13 @@
 // tests/api/health_check.rs
 
 // dependencies
-use actix_web::{http::header::ContentType, test, App};
+use actix_web::{test, App};
 use actix_web_template_lib::routes::health_check;
 
 #[actix_web::test]
-async fn health_check_works() {
+async fn test_health_check() {
   let app = test::init_service(App::new().service(health_check)).await;
-  let req = test::TestRequest::default()
-      .insert_header(ContentType::plaintext())
-      .to_request();
+  let req = test::TestRequest::get().uri("/health_check").to_request();
   let resp = test::call_service(&app, req).await;
   assert!(resp.status().is_success());
 }
